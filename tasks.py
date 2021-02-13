67d953ad4467d833adb0c1c99697b6a38741a78a
d-aughter <sagongrin.1@gmail.com>
2021-02-12 18:13:59 -0600

    finished dockerizing and added tasks for pyinvoke

---
+++ tasks.py
@@ -0,0 +1,53 @@
+import invoke
+
+
+@invoke.task(help={"arg": "(Takes docker-compose arguments. Use quotes for multiple arguments.)"})
+def compose(c, arg):
+    """
+    Acts as docker-compose
+    """
+    c.run(f"docker-compose -f docker/docker-compose.yml {arg}", pty=True)
+
+
+@invoke.task
+def exec(c):
+    """
+    Gets you into the docker container shell
+    """
+    compose(c, "exec stockbot /bin/bash")
+
+
+@invoke.task
+def build(c):
+    """
+    Builds the containers
+    """
+    compose(c, "build")
+
+
+@invoke.task(help={"verbose": "(Prints docker-compose log in real time)"})
+def dev(c, verbose=False):
+    """
+    One-button solution for getting the containers up
+    """
+    if verbose:
+        compose(c, "up")
+    else:
+        compose(c, "up -d")
+    print("Dev should be up.")
+
+
+@invoke.task
+def requirements(c):
+    """
+    Exports your pipenv environment to requirements.txt
+    """
+    c.run("pipenv run pip freeze > requirements.txt")
+
+
+@invoke.task
+def runbot(c):
+    """
+    Runs the bot
+    """
+    compose(c, "exec stockbot python /apps/stockbot/main.py")
