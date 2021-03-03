073f107cabb14fa611fb19cb9a2e88646fe531ae
d-aughter <sagongrin.1@gmail.com>
2021-03-02 23:16:43 -0600

    portfolio revamp completed

--- src/positions.py
+++ src/positions.py
@@ -67,6 +67,8 @@ def get_portfolio(session, user_id: str, username: str, mobile: bool):
         portfolio = discord.Embed(title=f"{username}'s Portfolio", colour=discord.Colour.green()) if mobile else []
         portfolio_total_usd = 0
         portfolio_total_cad = 0
+        average_total_usd = 0
+        average_total_cad = 0
         for pos in positions:
             symbol_id = pos.symbol_id
             symbol = session.query(db.Symbols).filter_by(symbol_id=symbol_id).one().symbol
@@ -78,17 +80,19 @@ def get_portfolio(session, user_id: str, username: str, mobile: bool):
             live_total_price = amount * live_price
             original_total_price = pos.total_price
             current_total_price = live_price * amount
-            if is_usd:
-                portfolio_total_usd += current_total_price
-            else:
-                portfolio_total_cad += current_total_price
             pl = live_total_price - original_total_price
             pl_percent = ((live_price - average_price) / average_price) * 100
+            if is_usd:
+                portfolio_total_usd += current_total_price
+                average_total_usd += average_price * amount
+            else:
+                portfolio_total_cad += current_total_price
+                average_total_cad += average_price * amount
             positive = "+" if pl > 0 else ""
-            pl = positive + neg_zero_handler(format(pl, '.2f'))
-            pl_percent = positive + f"{neg_zero_handler(format(pl_percent, '.2f'))}%"
-            average_price = format(average_price, '.2f')
-            current_total_price = format(current_total_price, '.2f')
+            pl = positive + neg_zero_handler(two_decimal(pl))
+            pl_percent = positive + f"{neg_zero_handler(two_decimal(pl_percent))}%"
+            average_price = two_decimal(average_price)
+            current_total_price = two_decimal(current_total_price)
             if mobile:
                 portfolio.add_field(name=f"**{symbol}**",
                                     value=f"> Amount: {amount}\n"
@@ -103,21 +107,34 @@ def get_portfolio(session, user_id: str, username: str, mobile: bool):
                                   current_total_price,
                                   f"{pl} ({pl_percent})",
                                   currency])
-        total_in_usd = format(portfolio_total_usd + convert("CAD", "USD", portfolio_total_cad), '.2f')
-        total_in_cad = format(convert("USD", "CAD", portfolio_total_usd) + portfolio_total_cad, '.2f')
-        portfolio_total_usd = format(portfolio_total_usd, '.2f')
-        portfolio_total_cad = format(portfolio_total_cad, '.2f')
+        total_in_usd, total_in_cad = get_total_usd_cad(portfolio_total_usd, portfolio_total_cad)
+        original_in_usd, original_in_cad = get_total_usd_cad(average_total_usd, average_total_cad)
+        pl_usd = total_in_usd - original_in_usd
+        pl_cad = total_in_cad - original_in_cad
+        pl_percent_in_usd = two_decimal((pl_usd / original_in_usd) * 100)
+        pl_percent_in_cad = two_decimal((pl_cad / original_in_cad) * 100)
+        portfolio_total_usd = two_decimal(portfolio_total_usd)
+        portfolio_total_cad = two_decimal(portfolio_total_cad)
+        positive = "+" if pl_usd > 0 or pl_cad > 0 else ""
         portfolio_total = [[portfolio_total_usd,
                             portfolio_total_cad,
-                            total_in_usd,
-                            total_in_cad]]
+                            two_decimal(total_in_usd),
+                            two_decimal(total_in_cad),
+                            f"{positive}{two_decimal(pl_usd)} ({positive}{pl_percent_in_usd}%)",
+                            f"{positive}{two_decimal(pl_cad)} ({positive}{pl_percent_in_cad}%)"
+                            ]]
         if mobile:
             portfolio_total_mobile = discord.Embed(title=f"{username}'s Portfolio Summary",
                                                    colour=discord.Colour.green())
             portfolio_total_mobile.add_field(name="Total USD", value=portfolio_total_usd)
             portfolio_total_mobile.add_field(name="Total CAD", value=portfolio_total_cad)
-            portfolio_total_mobile.add_field(name="Total in USD", value=total_in_usd)
-            portfolio_total_mobile.add_field(name="Total in CAD", value=total_in_cad)
+            portfolio_total_mobile.add_field(name="Total in USD", value=two_decimal(total_in_usd))
+            portfolio_total_mobile.add_field(name="Total in CAD", value=two_decimal(total_in_cad))
+            portfolio_total_mobile.add_field(name="P/L in USD",
+                                             value=f"{positive}{two_decimal(pl_usd)} ({positive}{pl_percent_in_usd}%)")
+            portfolio_total_mobile.add_field(name="P/L in CAD",
+                                             value=f"{positive}{two_decimal(pl_cad)} ({positive}{pl_percent_in_cad}%)")
+
             return portfolio, portfolio_total_mobile
 
         else:
@@ -125,7 +142,8 @@ def get_portfolio(session, user_id: str, username: str, mobile: bool):
                                        headers=["Symbol", "Amount", "Average", "Total", "P/L (%)", "Currency"],
                                        disable_numparse=True)
             portfolio_total_table = tabulate(portfolio_total,
-                                             headers=["Total USD", "Total CAD", "Total in USD", "Total in CAD"],
+                                             headers=["Total USD", "Total CAD", "Total in USD", "Total in CAD",
+                                                      "P/L in USD", "P/L in CAD"],
                                              disable_numparse=True)
             return portfolio_table, portfolio_total_table
     finally:
@@ -153,3 +171,13 @@ def convert(initial, final, amount):
 
 def neg_zero_handler(neg_zero):
     return "0.00" if neg_zero == "-0.00" else neg_zero
+
+
+def two_decimal(number: float):
+    return format(number, '.2f')
+
+
+def get_total_usd_cad(usd, cad):
+    total_in_usd = usd + convert("CAD", "USD", cad)
+    total_in_cad = cad + convert("USD", "CAD", usd)
+    return total_in_usd, total_in_cad
